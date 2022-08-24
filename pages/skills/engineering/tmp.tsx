05887eb78b52b6020c46956bb1990c4b03da75d1
Kwan Man Hei <kwanmanhei19980405@gmail.com>
2022-08-24 23:27:23 +0800

    responsive navbar (kind of)

---
+++ pages/skills/engineering/tmp.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styles from "../styles/tmp.module.css";
+
+const tmp = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <span></span>
+        <div className={styles.content}>
+          <h2>Card</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+            vitae, itaque asperiores deleniti tempore fugit inventore provident
+            quas explicabo iure, reiciendis voluptatem ipsum quibusdam illo
+            repellendus harum distinctio incidunt sint?
+          </p>
+          <a href="#">Read More</a>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <span></span>
+        <div className={styles.content}>
+          <h2>Card</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+            vitae, itaque asperiores deleniti tempore fugit inventore provident
+            quas explicabo iure, reiciendis voluptatem ipsum quibusdam illo
+            repellendus harum distinctio incidunt sint?
+          </p>
+          <a href="#">Read More</a>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <span></span>
+        <div className={styles.content}>
+          <h2>Card</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+            vitae, itaque asperiores deleniti tempore fugit inventore provident
+            quas explicabo iure, reiciendis voluptatem ipsum quibusdam illo
+            repellendus harum distinctio incidunt sint?
+          </p>
+          <a href="#">Read More</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default tmp;
