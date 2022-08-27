a623c8943e1237121b0d9c40ec9230d16919eea0
Kwan Man Hei <kwanmanhei19980405@gmail.com>
2022-08-27 12:19:06 +0800

    new engineering page in progress

--- pages/skills/engineering/tmp.tsx
+++ pages/skills/engineering/tmp.tsx
@@ -3,57 +3,64 @@ import styles from "../styles/tmp.module.css";
 
 const tmp = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.card}>
-        <span></span>
-        <div className={styles.content}>
-          <h2>Card</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-            vitae, itaque asperiores deleniti tempore fugit inventore provident
-            quas explicabo iure, reiciendis voluptatem ipsum quibusdam illo
-            repellendus harum distinctio incidunt sint?
-          </p>
-          <a href="#">Read More</a>
-        </div>
+    <div className={styles.box}>
+      <div className={styles.title}>
+        I am an A.I. Engineer, with some knowledge in Blockchain and Web
+        Development. <br />
+        Of course, a bit of DevOps knowledge is always needed.
       </div>
-      <div className={styles.card}>
-        <span></span>
-        <div className={styles.content}>
-          <h2>Card</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-            vitae, itaque asperiores deleniti tempore fugit inventore provident
-            quas explicabo iure, reiciendis voluptatem ipsum quibusdam illo
-            repellendus harum distinctio incidunt sint?
-          </p>
-          <a href="#">Read More</a>
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <span></span>
+          <div className={styles.content}>
+            <h2>Artificial Intelligence</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+              vitae, itaque asperiores deleniti tempore fugit inventore
+              provident quas explicabo iure, reiciendis voluptatem ipsum
+              quibusdam illo repellendus harum distinctio incidunt sint?
+            </p>
+            {/* <a href="#">My Tech Stack</a> */}
+          </div>
         </div>
-      </div>
-      <div className={styles.card}>
-        <span></span>
-        <div className={styles.content}>
-          <h2>Card</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-            vitae, itaque asperiores deleniti tempore fugit inventore provident
-            quas explicabo iure, reiciendis voluptatem ipsum quibusdam illo
-            repellendus harum distinctio incidunt sint?
-          </p>
-          <a href="#">Read More</a>
+        <div className={styles.card}>
+          <span></span>
+          <div className={styles.content}>
+            <h2>Blockchain</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+              vitae, itaque asperiores deleniti tempore fugit inventore
+              provident quas explicabo iure, reiciendis voluptatem ipsum
+              quibusdam illo repellendus harum distinctio incidunt sint?
+            </p>
+            {/* <a href="#">My Tech Stack</a> */}
+          </div>
         </div>
-      </div>
-      <div className={styles.card}>
-        <span></span>
-        <div className={styles.content}>
-          <h2>Card</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-            vitae, itaque asperiores deleniti tempore fugit inventore provident
-            quas explicabo iure, reiciendis voluptatem ipsum quibusdam illo
-            repellendus harum distinctio incidunt sint?
-          </p>
-          <a href="#">Read More</a>
+        <div className={styles.card}>
+          <span></span>
+          <div className={styles.content}>
+            <h2>Web Development</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+              vitae, itaque asperiores deleniti tempore fugit inventore
+              provident quas explicabo iure, reiciendis voluptatem ipsum
+              quibusdam illo repellendus harum distinctio incidunt sint?
+            </p>
+            {/* <a href="#">My Tech Stack</a> */}
+          </div>
+        </div>
+        <div className={styles.card}>
+          <span></span>
+          <div className={styles.content}>
+            <h2>DevOps</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+              vitae, itaque asperiores deleniti tempore fugit inventore
+              provident quas explicabo iure, reiciendis voluptatem ipsum
+              quibusdam illo repellendus harum distinctio incidunt sint?
+            </p>
+            {/* <a href="#">My Tech Stack</a> */}
+          </div>
         </div>
       </div>
     </div>
