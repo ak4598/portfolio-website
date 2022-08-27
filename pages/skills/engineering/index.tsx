2d823b8d45c2effb773e6bfe0f75557665f5fc8f
Kwan Man Hei <kwanmanhei19980405@gmail.com>
2022-08-27 22:32:45 +0800

    new engeering page done

--- pages/skills/engineering/index.tsx
+++ pages/skills/engineering/index.tsx
@@ -1,23 +1,74 @@
 import React from "react";
 import styles from "../styles/engineering.module.css";
-import NeonCards from "../../../components/NeonCards/NeonCards";
-import * as lib from "../../../assets/LibLogos";
-import * as pro from "../../../assets/ProgLogos";
-import * as framework from "../../../assets/FrameworkLogos";
-import * as other from "../../../assets/OtherLogos";
+import { AI, Blockchain, Web, DevOps } from "../../../assets/TechLogos";
 
-const Engineering = () => {
+const tmp = () => {
   return (
-    <div>
-      <div className={styles.background}>
-        <div className={styles.content}>
-          <div className={styles.cards}>
-            <NeonCards />
+    <div className={styles.box}>
+      <div className={styles.title}>
+        I am an A.I. Engineer, with some knowledge in Blockchain and Web
+        Development. <br />
+        Of course, having a bit of DevOps knowledge is always appreciated.
+      </div>
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <span className={styles.span1}></span>
+          <span className={styles.span2}></span>
+          <div className={styles.content}>
+            <img src={AI.src} />
+            <h2>Artificial Intelligence</h2>
+            <p>
+              Extensive knowledge of the end-to-end delivery on solutions in
+              both CV and NLP.
+            </p>
+            <a href="/skills/engineering/ai">
+              <button>My Tech Stack</button>
+            </a>
           </div>
-          <div className={styles.title}>
-            I am an A.I. Engineer, with some knowledge in Blockchain and Web
-            Development. <br />
-            Of course, a bit of DevOps knowledge is always needed.
+        </div>
+        <div className={styles.card}>
+          <span className={styles.span1}></span>
+          <span className={styles.span2}></span>
+          <div className={styles.content}>
+            <img src={Blockchain.src} />
+            <h2>Blockchain</h2>
+            <p>
+              Knowledge on both public chain and private chain. Blockchain is
+              more than cryptocurrency.
+            </p>
+            <a href="/skills/engineering/blockchain">
+              <button>My Tech Stack</button>
+            </a>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <span className={styles.span1}></span>
+          <span className={styles.span2}></span>
+          <div className={styles.content}>
+            <img src={Web.src} />
+            <h2>Web Development</h2>
+            <p>
+              JavaScript is everywhere now. Building websites is just a way for
+              me to survive as an engineer :{"("}
+            </p>
+            <a href="/skills/engineering/web">
+              <button>My Tech Stack</button>
+            </a>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <span className={styles.span1}></span>
+          <span className={styles.span2}></span>
+          <div className={styles.content}>
+            <img src={DevOps.src} />
+            <h2>DevOps</h2>
+            <p>
+              Experienced in containerizing applications. And yes, I know
+              regular expression :{")"}
+            </p>
+            <a href="/skills/engineering/devops">
+              <button>My Tech Stack</button>
+            </a>
           </div>
         </div>
       </div>
@@ -25,4 +76,4 @@ const Engineering = () => {
   );
 };
 
-export default Engineering;
+export default tmp;
