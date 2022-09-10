6f506a501b1fcdfba732a3fa2cda2da497e9fc05
Kwan Man Hei <kwanmanhei19980405@gmail.com>
2022-09-10 16:08:20 +0800

    workale react-globe.gl

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,28 @@
+import React, { useState, useEffect } from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import NavBar from "../components/NavBar/NavBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <div>
-      <NavBar />
-      <Component {...pageProps} />
-    </div>
-  );
+  const [showChild, setShowChild] = useState(false);
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if (!showChild) {
+    return null;
+  }
+
+  if (typeof window === "undefined") {
+    return <></>;
+  } else {
+    return (
+      <div>
+        <NavBar />
+        <Component {...pageProps} />
+      </div>
+    );
+  }
 }
 
 export default MyApp;
--- pages/home/about.tsx
+++ pages/home/about.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import styles from "./styles/about.module.css";
+import texture from "../../assets/images/Blue_Watercolor_Map_of_the_World.jpg";
 
 const About = () => {
-  return <div>About</div>;
+  let Globe = () => null;
+
+  if (typeof window !== "undefined") {
+    Globe = require("react-globe.gl").default;
+  }
+
+  const earth = useRef<HTMLDivElement | null>(null);
+
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    setImageUrl(texture.src);
+  }, []);
+
+  return (
+    <div className={styles.scene}>
+      <div className={styles.earth}>
+        <Globe
+          //@ts-ignore
+          ref={earth}
+          width={900}
+          height={900}
+          backgroundColor={"rgba(0,0,0,0)"}
+          globeImageUrl={imageUrl}
+          arcColor={"color"}
+          arcDashGap={0.6}
+          arcDashLength={0.3}
+          arcDashAnimateTime={4000 + 500}
+          rendererConfig={{ preserveDrawingBuffer: true }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default About;
