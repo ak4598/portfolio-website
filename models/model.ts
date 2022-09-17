2136598520cb68f5e5a4172c199e31ab8c449de7
Kwan Man Hei <kwanmanhei19980405@gmail.com>
2022-09-17 20:01:24 +0800

    add log

--- models/model.ts
+++ models/model.ts
@@ -1,17 +1,11 @@
 import { Schema, model, models } from "mongoose";
 
-const userSchema = new Schema({
-  name: { type: String, required: true },
-  password: { type: String, required: true },
-});
-
 const countrySchema = new Schema({
   country: { type: String, required: true },
   caption: { type: String, required: true },
   image: { type: String, required: true },
 });
 
-const userModel = models.User || model("User", userSchema);
 const countryModel = models.Country || model("Country", countrySchema);
 
-export { userModel, countryModel };
+export { countryModel };
