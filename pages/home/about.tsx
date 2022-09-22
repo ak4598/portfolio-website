ed00effc765b151659a71f16e735e9f343f5f061
Kwan Man Hei <kwanmanhei19980405@gmail.com>
2022-09-22 09:20:55 +0800

    add get scroll y, get window size

--- pages/home/about.tsx
+++ pages/home/about.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useCallback, useState } from "react";
 import Image from "next/image";
 import { about1, about2, about3, about4 } from "../../assets/images";
 import styles from "./styles/about.module.css";
@@ -10,43 +10,62 @@ import {
 } from "react-scroll-parallax";
 
 const About = () => {
+  const [scrollY, setScrollY] = useState<number>(0);
+  const [windowSize, setWindowSize] = useState<{ [name: string]: number }>({
+    width: 0,
+    height: 0,
+  });
+
+  const handleResize = () => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+    console.log("width", windowSize.width, "height", windowSize.height);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [handleResize]);
+
+  const onScroll = useCallback(() => {
+    const { pageYOffset, scrollY } = window;
+    console.log("yOffset", pageYOffset, "scrollY", scrollY);
+    setScrollY(window.pageYOffset);
+  }, []);
+
+  useEffect(() => {
+    //add eventlistener to window
+    window.addEventListener("scroll", onScroll, { passive: true });
+    // remove event on unmount to prevent a memory leak with the cleanup
+    return window.removeEventListener("scroll", onScroll);
+  }, []);
+
   return (
     <div className={styles.top}>
-      <ParallaxBanner className={styles.parallaxBanner}>
-        <ParallaxBannerLayer image={about1.src} speed={0} />
-        {/* <Parallax speed={-20}>
-          <div className={styles.title}>This is Andy Kwan.</div>
+      <div className={styles.banner}>
+        <Image src={about1.src} layout="fill" objectFit="cover" quality={100} />
+        <div className={styles.title}>
+          This is Andy Kwan.
           <div className={styles.arrow}>
             <span></span>
             <span></span>
             <span></span>
           </div>
-        </Parallax>
-        <Parallax speed={-20}>
-          <Parallax translateX={[300, -10]} easing="easeOutQuad" speed={-10}>
-            <div className={styles.box} id={styles.intro1}>
-              <p>He is currently a consultant at Deloitte FinTech Lab.</p>
-            </div>
-          </Parallax>
-          <Parallax translateX={[-300, 10]} easing="easeOutQuad" speed={-10}>
-            <div className={styles.box} id={styles.intro2}>
-              <p>
-                He focuses on delivering end-to-end and state-of-the-art A.I.
-                solutions for his client.
-              </p>
-            </div>
-          </Parallax>
-        </Parallax> */}
-      </ParallaxBanner>
-      <ParallaxBanner className={styles.parallaxBanner}>
-        <ParallaxBannerLayer image={about2.src} speed={0} />
-      </ParallaxBanner>
-      <ParallaxBanner className={styles.parallaxBanner}>
-        <ParallaxBannerLayer image={about3.src} speed={0} />
-      </ParallaxBanner>
-      <ParallaxBanner className={styles.parallaxBanner}>
-        <ParallaxBannerLayer image={about4.src} speed={0} />
-      </ParallaxBanner>
+        </div>
+      </div>
+      <div className={styles.banner}>
+        <Image src={about2.src} layout="fill" objectFit="cover" quality={100} />
+      </div>
+      <div className={styles.banner}>
+        <Image src={about3.src} layout="fill" objectFit="cover" quality={100} />
+      </div>
+      <div className={styles.banner}>
+        <Image src={about4.src} layout="fill" objectFit="cover" quality={100} />
+      </div>
+      {/* He is currently a consultant at Deloitte FinTech Lab. */}
+      {/* He focuses on delivering end-to-end and state-of-the-art A.I. solutions for his client. */}
     </div>
   );
 };
