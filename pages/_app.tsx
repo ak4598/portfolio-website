b6b4202baa5a6da5fe46d9555debcb2f82adf649
Kwan Man Hei <kwanmanhei19980405@gmail.com>
2022-10-02 22:11:07 +0800

    test spinner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,41 @@
 import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import Head from "next/head";
 import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import NavBar from "../components/NavBar/NavBar";
+
+function Loading(): any {
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = (url: string) =>
+      url !== router.asPath && setLoading(true);
+    const handleComplete = (url: string) =>
+      url === router.asPath && setLoading(false);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  });
+
+  return (
+    loading && (
+      <div className="spinner-wrapper">
+        <div className="spinner"></div>
+      </div>
+    )
+  );
+}
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const [showChild, setShowChild] = useState(false);
@@ -20,13 +52,16 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     return <></>;
   } else {
     return (
-      <SessionProvider session={session}>
-        <Head>
-          <title>Andy Kwan</title>
-        </Head>
-        <NavBar />
-        <Component {...pageProps} />
-      </SessionProvider>
+      <>
+        <Loading />
+        <SessionProvider session={session}>
+          <Head>
+            <title>Andy Kwan</title>
+          </Head>
+          <NavBar />
+          <Component {...pageProps} />
+        </SessionProvider>
+      </>
     );
   }
 }
