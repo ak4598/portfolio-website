3e2a290845ecc964936a41fbb01408adcef9b95e
Kwan Man Hei <kwanmanhei19980405@gmail.com>
2022-10-04 22:43:06 +0800

    auth only allow me

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,5 +9,19 @@ export const authOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    //@ts-ignore
+    async signIn({ user }) {
+      const isAllowedToSignIn =
+        user.email === process.env.GITHUB_ID_EMAIL &&
+        user.id === process.env.GITHUB_ID_ID;
+      console.log(isAllowedToSignIn);
+      if (isAllowedToSignIn) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
 };
 export default NextAuth(authOptions);
