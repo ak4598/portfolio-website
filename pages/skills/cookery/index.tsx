8fe92551a553789c8122d034495c2d1915228ae4
Kwan Man Hei <kwanmanhei19980405@gmail.com>
2022-10-04 12:55:21 +0800

    fix landscape orientation small screen, start cookery

---
+++ pages/skills/cookery/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import type { NextPage } from "next";
+import styles from "../styles/cookery.module.css";
+
+const Cookery: NextPage = () => {
+  return (
+    <div>
+      <div className={styles.background}>
+        <span></span>
+        <span></span>
+      </div>
+      <div className={styles.title}>Cookery</div>
+    </div>
+  );
+};
+
+export default Cookery;
