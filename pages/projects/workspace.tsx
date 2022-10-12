023b8c48b5cc944f530b2f3f2e1476da5f7773f2
Kwan Man Hei <kwanmanhei19980405@gmail.com>
2022-10-12 23:00:52 +0800

    image optimization

--- pages/projects/workspace.tsx
+++ pages/projects/workspace.tsx
@@ -8,6 +8,7 @@ import { GitHub } from "../../assets/SocialMediaLogos";
 import Slider from "../../components/Slider/Slider";
 import ProjectCard from "../../components/ProjectCard/ProjectCard";
 import type { project } from "../../assets/Projects/projects";
+import { black } from "../../assets/images";
 
 type Props = {
   project: project;
@@ -62,7 +63,7 @@ const Background: React.FC<Props> = ({ project }) => {
     <div className={styles.backgroundContainer} key={project.title + "bg"}>
       <Image
         src={project.thumbnail as string}
-        blurDataURL={project.thumbnail as string}
+        blurDataURL={black.src}
         placeholder="blur"
         layout="fill"
         objectFit="cover"
