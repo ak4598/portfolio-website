687f332f2d8c2e5a6da7edc0536ca4a4ef6729aa
Kwan Man Hei <kwanmanhei19980405@gmail.com>
2022-10-15 18:57:35 +0800

    roll back no getServerSideProps

--- pages/skills/cookery/index.tsx
+++ pages/skills/cookery/index.tsx
@@ -12,33 +12,34 @@ type Props = {
   posts: any;
 };
 
-const Cookery: React.FC<Props> = ({ posts }) => {
-  // const [posts, setPosts] = useState<any>(null);
-  // const [isLoading, setLoading] = useState<boolean>(false);
-  // useEffect(() => {
-  //   setLoading(true);
+// const Cookery: React.FC<Props> = ({ posts }) => {
+const Cookery: NextPage = () => {
+  const [posts, setPosts] = useState<any>(null);
+  const [isLoading, setLoading] = useState<boolean>(false);
+  useEffect(() => {
+    setLoading(true);
 
-  //   fetch("/api/images/get-food-images")
-  //     .then((res) => res.json())
-  //     .then((id) => {
-  //       shuffle(id);
-  //       setPosts(id);
-  //       setLoading(false);
-  //     });
-  // }, []);
+    fetch("/api/images/get-food-images")
+      .then((res) => res.json())
+      .then((id) => {
+        shuffle(id);
+        setPosts(id);
+        setLoading(false);
+      });
+  }, []);
 
-  // if (isLoading || posts === null) {
-  //   return (
-  //     <div className={styles.background}>
-  //       <div className={styles.title}>
-  //         Just a home chef that needs to serve his family every day👨🏻‍🍳
-  //       </div>
-  //       <div className={styles.loading}>
-  //         <Image src={Loading.src} width={200} height={200} priority={true} />
-  //       </div>
-  //     </div>
-  //   );
-  // }
+  if (isLoading || posts === null) {
+    return (
+      <div className={styles.background}>
+        <div className={styles.title}>
+          Just a home chef that needs to serve his family every day👨🏻‍🍳
+        </div>
+        <div className={styles.loading}>
+          <Image src={Loading.src} width={200} height={200} priority={true} />
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className={styles.background}>
@@ -61,14 +62,14 @@ const Cookery: React.FC<Props> = ({ posts }) => {
 
 export default Cookery;
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  // Fetch data from external API
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   // Fetch data from external API
 
-  const host = context.req.headers.host;
-  const res = await fetch(`http://${host}/api/images/get-food-images`);
-  const data = await res.json();
-  const posts = shuffle(data);
+//   const host = context.req.headers.host;
+//   const res = await fetch(`http://${host}/api/images/get-food-images`);
+//   const data = await res.json();
+//   const posts = shuffle(data);
 
-  // Pass data to the page via props
-  return { props: { posts } };
-};
+//   // Pass data to the page via props
+//   return { props: { posts } };
+// };
