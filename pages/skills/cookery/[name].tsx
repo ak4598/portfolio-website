4083bf791a2a2ce6905506dddf295af97a9c33f2
Kwan Man Hei <kwanmanhei19980405@gmail.com>
2022-10-15 10:02:36 +0800

    recipe add in viewpoint enter

--- pages/skills/cookery/[name].tsx
+++ pages/skills/cookery/[name].tsx
@@ -9,8 +9,14 @@ import Error from "../../404";
 import { Cook, Cookf, Clock } from "../../../assets/OtherLogos";
 
 const Ingredient: React.FC<{ ingredient: string }> = ({ ingredient }) => {
+  const { ref, inView } = useInView();
+
   return (
-    <div>
+    <div
+      ref={ref}
+      className={`${styles.ingredient}`}
+      style={inView ? { opacity: "1", transform: "translateX(0%)" } : {}}
+    >
       <h2>{ingredient.split(",")[0]}</h2>
       <p>{ingredient.split(",")[1]}</p>
     </div>
@@ -18,12 +24,20 @@ const Ingredient: React.FC<{ ingredient: string }> = ({ ingredient }) => {
 };
 
 const Step: React.FC<{ idx: number; step: string }> = ({ idx, step }) => {
+  const { ref, inView } = useInView();
+
   return (
-    <div className={styles.step}>
-      <div className={styles.number}>
+    <div className={styles.step} ref={ref}>
+      <div
+        className={styles.number}
+        style={inView ? { opacity: "1", transform: "translateX(0%)" } : {}}
+      >
         {idx + 1 < 10 ? <h2>0{idx + 1}</h2> : <h2>{idx + 1}</h2>}
       </div>
-      <div className={styles.description}>
+      <div
+        className={styles.description}
+        style={inView ? { opacity: "1", transform: "translateX(0%)" } : {}}
+      >
         <p>
           {step.split("<br />").map((i, idx) => (
             <span key={idx}>
@@ -42,6 +56,8 @@ const Recipe: NextPage = () => {
   const [isLoading, setLoading] = useState<boolean>(false);
   const [hasRecipe, setHasRecipe] = useState<boolean>(false);
 
+  const { ref: difficultyRef, inView: difficultyVisible } = useInView();
+  const { ref: timeRef, inView: timeVisible } = useInView();
   const { ref: ingredientsRef, inView: ingredientsVisible } = useInView();
   const { ref: stepsRef, inView: stepsVisible } = useInView();
   const { ref: backRef, inView: backVisible } = useInView();
@@ -92,7 +108,15 @@ const Recipe: NextPage = () => {
         </div>
       </div>
       <div className={styles.details1}>
-        <div className={styles.difficulty}>
+        <div
+          className={styles.difficulty}
+          ref={difficultyRef}
+          style={
+            difficultyVisible
+              ? { opacity: "1", transform: "translateX(0%)" }
+              : {}
+          }
+        >
           <div className={styles.icon}></div>
           {[...Array(post.difficulty)].map((id, idx) => (
             <div key={idx} className={styles.icon}>
@@ -106,7 +130,13 @@ const Recipe: NextPage = () => {
           ))}
           <div className={styles.icon}></div>
         </div>
-        <div className={styles.time}>
+        <div
+          className={styles.time}
+          ref={timeRef}
+          style={
+            timeVisible ? { opacity: "1", transform: "translateX(0%)" } : {}
+          }
+        >
           <div className={styles.icon}>
             <Image src={Clock.src} layout="fill" />
           </div>
@@ -124,36 +154,52 @@ const Recipe: NextPage = () => {
         )}
         {hasRecipe && (
           <div className={styles.box}>
-            <div
-              className={`${styles.section} ${
-                ingredientsVisible ? styles.show : ""
-              }`}
-              ref={ingredientsRef}
-            >
-              <h1>Ingredients</h1>
+            <div className={`${styles.section}`}>
+              <h1
+                style={
+                  ingredientsVisible
+                    ? { opacity: "1", transform: "translateX(0%)" }
+                    : {}
+                }
+                ref={ingredientsRef}
+              >
+                Ingredients
+              </h1>
               <div className={styles.ingredients}>
                 {post.ingredients.map((ingredient: string) => (
                   <Ingredient key={ingredient} ingredient={ingredient} />
                 ))}
               </div>
             </div>
-            <div
-              className={`${styles.section} ${stepsVisible ? styles.show : ""}`}
-              ref={stepsRef}
-            >
-              <h1>Steps</h1>
+            <div className={`${styles.section}`}>
+              <h1
+                ref={stepsRef}
+                style={
+                  stepsVisible
+                    ? { opacity: "1", transform: "translateX(0%)" }
+                    : {}
+                }
+              >
+                Steps
+              </h1>
               <div className={styles.steps}>
                 {post.steps.map((step: string, idx: number) => (
                   <Step key={step} idx={idx} step={step} />
                 ))}
               </div>
             </div>
-            <div
-              className={`${styles.section} ${backVisible ? styles.show : ""}`}
-              ref={backRef}
-            >
+            <div className={`${styles.backContainer}`} ref={backRef}>
               <Link href="/skills/cookery">
-                <button className={styles.back}>Back</button>
+                <button
+                  className={styles.back}
+                  style={
+                    backVisible
+                      ? { opacity: "1", transform: "translateX(0%)" }
+                      : {}
+                  }
+                >
+                  Back
+                </button>
               </Link>
             </div>
           </div>
