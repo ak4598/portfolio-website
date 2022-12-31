b47e7b90e1cfc5dd2c96ee3a884453f301184220
Kwan Man Hei <kwanmanhei19980405@gmail.com>
2022-12-31 15:43:45 +0800

    test getStaticProps

--- pages/api/images/get-food-images.ts
+++ pages/api/images/get-food-images.ts
@@ -13,7 +13,8 @@ export default async function getCountry(
   }
 
   try {
-    const imagesId = await FoodModel.find({}, "_id");
+    // const imagesId = await FoodModel.find({}, "_id");
+    const imagesId = await FoodModel.find({});
 
     res.status(200).json(imagesId);
   } catch (error: any) {
--- pages/skills/cookery/index.tsx
+++ pages/skills/cookery/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import type { NextPage, GetServerSideProps } from "next";
+import type { NextPage, GetServerSideProps, GetStaticProps } from "next";
 import styles from "../styles/cookery.module.css";
 import FoodCard from "../../../components/FoodCard/FoodCard";
 
@@ -11,49 +11,49 @@ import { Loading } from "../../../assets/OtherLogos";
 import shuffle from "../../../utils/shuffle";
 
 type Props = {
-  posts: any;
+  data: any;
 };
 
-// const Cookery: React.FC<Props> = ({ posts }) => {
-const Cookery: NextPage = () => {
-  const [posts, setPosts] = useState<any>(null);
+const Cookery: React.FC<Props> = ({ data }) => {
+  // const Cookery: NextPage = () => {
+  // const [posts, setPosts] = useState<any>(null);
   // const [isLoading, setLoading] = useState<boolean>(false);
 
-  const fetchData = async () => {
-    fetch("/api/images/get-food-images")
-      .then((res) => res.json())
-      .then((id) => {
-        shuffle(id);
-        setPosts(id);
-        // setLoading(false);
-      });
-  };
+  // const fetchData = async () => {
+  //   fetch("/api/images/get-food-images")
+  //     .then((res) => res.json())
+  //     .then((id) => {
+  //       shuffle(id);
+  //       setPosts(id);
+  //       // setLoading(false);
+  //     });
+  // };
 
-  const { isError, isSuccess, isLoading, data, error } = useQuery(
-    ["posts"],
-    fetchData,
-    { staleTime: 60000 }
-  );
-
-  console.log();
+  // const { isError, isSuccess, isLoading, data, error } = useQuery(
+  //   ["posts"],
+  //   fetchData,
+  //   { staleTime: 60000 }
+  // );
 
   // useEffect(() => {
   //   setLoading(true);
   //   fetchData();
   // }, []);
 
-  if (isLoading || posts === null) {
-    return (
-      <div className={styles.background}>
-        <div className={styles.title}>
-          Just a home chef that needs to serve his family every day👨🏻‍🍳
-        </div>
-        <div className={styles.loading}>
-          <Image src={Loading.src} width={200} height={200} priority={true} />
-        </div>
-      </div>
-    );
-  }
+  // if (isLoading || posts === null) {
+  //   return (
+  //     <div className={styles.background}>
+  //       <div className={styles.title}>
+  //         Just a home chef that needs to serve his family every day👨🏻‍🍳
+  //       </div>
+  //       <div className={styles.loading}>
+  //         <Image src={Loading.src} width={200} height={200} priority={true} />
+  //       </div>
+  //     </div>
+  //   );
+  // }
+
+  const posts = shuffle(data);
 
   return (
     <div className={styles.background}>
@@ -65,7 +65,16 @@ const Cookery: NextPage = () => {
         <div className={styles.grid}>
           {posts.map((post: any) => (
             <div key={post._id} className={styles.gridItem}>
-              <FoodCard id={post._id} />
+              <FoodCard
+                cuisine={post.cuisine}
+                difficulty={post.difficulty}
+                image={post.image}
+                ingredients={post.ingredients}
+                name={post.name}
+                steps={post.steps}
+                time={post.time}
+                _id={post._id}
+              />
             </div>
           ))}
         </div>
@@ -76,14 +85,14 @@ const Cookery: NextPage = () => {
 
 export default Cookery;
 
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   // Fetch data from external API
+export const getStaticProps: GetStaticProps = async () => {
+  // export const getServerSideProps: GetServerSideProps = async (context) => {
 
-//   const host = context.req.headers.host;
-//   const res = await fetch(`http://${host}/api/images/get-food-images`);
-//   const data = await res.json();
-//   const posts = shuffle(data);
+  const host = process.env.HOST;
+  const res = await fetch(`http://${host}/api/images/get-food-images`);
+  const data = await res.json();
+  // const posts = shuffle(data);
 
-//   // Pass data to the page via props
-//   return { props: { posts } };
-// };
+  // Pass data to the page via props
+  return { props: { data } };
+};
